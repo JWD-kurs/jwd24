37ac9501b8ed6d99cd6d7be9e55ded2e3d2b27d5
nenadtodEdu <n.todor@outlook.com>
2017-11-25 17:57:17 +0100

    Dodat lab5-end

---
+++ lab5-end/src/main/java/jwd/wafepa/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package jwd.wafepa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import jwd.wafepa.model.User;
+
+@Repository
+public interface UserRepository 
+	extends PagingAndSortingRepository<User, Long> {
+
+}
